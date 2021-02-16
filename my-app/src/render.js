178ed98bbef383aba9746a208b3bb9e5f730fed0
randyaajr <randyaajrsp@gmail.com>
2021-02-16 22:10:17 +0000

    basic logic started

---
+++ my-app/src/render.js
@@ -0,0 +1,11 @@
+//buttons
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+
+
+const { desktopCapturer, remote } = require('electron');
+
+// Get available video sources
+function getVideoSources() {}
